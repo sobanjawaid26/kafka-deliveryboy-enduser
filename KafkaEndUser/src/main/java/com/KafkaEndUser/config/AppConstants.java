115d70f939fd1fb020d6549d10acdb57edac05ae
thawbaan <sobanjawaid26@gmail.com>
2024-04-26 19:47:11 +0530

    Kafka Consumer End User created

---
+++ KafkaEndUser/src/main/java/com/KafkaEndUser/config/AppConstants.java
@@ -0,0 +1,7 @@
+package com.KafkaEndUser.config;
+
+public class AppConstants {
+
+    public static final String LOCATION_TOPIC_NAME = "location-update-topic";
+    public static final String GROUP_ID = "group-1";
+}
